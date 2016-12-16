cc4a7721942e1be3e1e4f1c3da82f13e7513a2a0
Marc Jakobi <jakobi@htw-berlin.de>
2016-12-16 20:59:34 +0100

    added assertion test

--- MLUnitTests/dischargeFitTests/dischargeFitTests.m
+++ MLUnitTests/dischargeFitTests/dischargeFitTests.m
@@ -18,22 +18,48 @@ Cex = 0.1;
 x0 = -3;
 v0 = 1400;
 delta = 260;
+x0 = [E0; Ea; Eb; Aex; Bex; Cex; x0; v0; delta];
 
 %% Args
 Temp = const.T_room;
 CRate = 1;
 
 %% Initialize with params
-d = dischargeFit(V, C_d, CRate, Temp, E0, Ea, Eb, Aex, Bex, Cex, x0, v0, delta);
+d = dischargeFit(V, C_d, CRate, Temp, 'x0', x0, 'mode', 'lsq');
 if fig
     d.plotResults
 end
 %% Initialize without params
-d2 = dischargeFit(V, C_d, CRate, Temp);
+d2 = dischargeFit(V, C_d, CRate, Temp, 'mode', 'lsq');
 if fig
     d2.plotResults
 end
 
+%% fminsearch options
+d3 = dischargeFit(V, C_d, CRate, Temp, 'x0', x0, 'mode', 'fmin');
+if fig
+    d3.plotResults
+end
+d4 = dischargeFit(V, C_d, CRate, Temp, 'mode', 'fmin');
+if fig
+    d4.plotResults
+end
+
+%% switch modes
+d2.mode = 'fmin';
+if fig
+   d2.plotResults
+end
+
+%% both
+dischargeFit(V, C_d, CRate, Temp, 'mode', 'both'); % validate syntax
+d = dischargeFit(V, C_d, CRate, Temp); % 'both' should be default
+if fig
+    d.plotResults
+end
+
 % TODO: Create assertion tests
+assert(isequal(d.rmse, d2.rmse), 'mode switch or ''both'' mode not functioning as expected')
+
 
 end
