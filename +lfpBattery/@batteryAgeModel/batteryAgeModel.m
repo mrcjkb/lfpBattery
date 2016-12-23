b8658b5439db77a18e68fd46b29da0222faa3f41
Marc Jakobi <jakobi@htw-berlin.de>
2016-12-23 14:40:56 +0100

    added init_soh to constructor

--- +lfpBattery/@batteryAgeModel/batteryAgeModel.m
+++ +lfpBattery/@batteryAgeModel/batteryAgeModel.m
@@ -1,7 +1,9 @@
 classdef (Abstract) batteryAgeModel < handle % MTODO: make abstract
     %BATTERYAGEMODEL Abstract class for modelling the aging of a battery.
     %Notifies event listeners every time SoH changes. Use this class's
-    %addlistener() method to add event listeners.
+    %addlistener() method to add event listeners for the SohChanged and EolReached events.
+    %
+    %Authors: Marc Jakobi, Festus Anyangbe, Marc Schmidt, December 2016
     
     properties (Hidden, SetAccess = 'protected')
         Ac; % Total age loss [0..1]
@@ -17,12 +19,26 @@ classdef (Abstract) batteryAgeModel < handle % MTODO: make abstract
         EolReached; % Notify listeners that the end of life specified by eolSoH has been reached
     end
     methods
-        % Constructor
-        function b = batteryAgeModel(eols)
+        function b = batteryAgeModel(eols, init_age)
+            %BATTERYAGEMODEL Constructor:
+            %
+            %   b = batteryAgeModel;                creates a batteryAgeModel object
+            %                                       with an end of life SoH of 0.2
+            %   b = batteryAgeModel(eol);           creates a batteryAgeModel object
+            %                                       with an end of life SoH
+            %                                       specified by eol
+            %   b = batteryAgeModel(eol, init_age); initializes the age
+            %                                       with init_age
             if nargin < 1
                 eols = 0.2;
+                init_age = 0;
+            elseif nargin < 2
+                init_age = 0;
             end
+            errChks.onezeroChk(eols, 'eols')
+            errChks.onezeroChk(init_age, 'init_age')
             b.eolSoH = eols;
+            b.Ac = init_age;
         end
         % Dependent setters
         function set.Ac(b, a)
@@ -33,9 +49,7 @@ classdef (Abstract) batteryAgeModel < handle % MTODO: make abstract
             end
         end
         function set.eolSoH(b, soh)
-            if soh > 1 || soh < 0
-                error('End of life state of health must be a value between 0 and 1')
-            end
+            errChks.onezeroChk(soh, 'End of life state of health')
             b.eolAc = 1 - soh;
         end
         % Dependent getters
