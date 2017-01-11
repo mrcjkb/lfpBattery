3a0c79f5bbb39a0c5c0ca71b47121fdc2fff2067
Marc Jakobi <jakobi@htw-berlin.de>
2017-01-11 22:29:07 +0100

    added sortedFunctions and curvefitCollection

--- +lfpBattery/@curvefitCollection/curvefitCollection.m
+++ +lfpBattery/@curvefitCollection/curvefitCollection.m
@@ -1,14 +1,26 @@
-classdef curvefitCollection < lfpBattery.sortedCollection
+classdef curvefitCollection < lfpBattery.sortedFunctions
     %CURVEFITCOLLECTION sorted collection of curveFit objects. Uses
     %interplation to extract data between two curve fits according to z
     %value
-    
+    %
+    %curvefitCollection Properties:
+    %
+    %   xydata - curve fit objects (should implement curveFitInterface)
+    %   z      - z data at which the respective curve measurements were
+    %            recorded (i. e. temperature, current,...)
+    %
+    %
+    %Authors: Marc Jakobi, Festus Anyangbe, Marc Schmidt
+    %         January 2017
     properties
     end
     
     methods
         function c = curvefitCollection(varargin)
-            c@lfpBattery.sortedCollection(varargin{:})
+            for i = 1:numel(varargin)
+                lfpBattery.commons.validateInterface(varargin{i}, 'lfpBattery.curveFitInterface')
+            end
+            c@lfpBattery.sortedFunctions(varargin{:})
         end
     end
     
