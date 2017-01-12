dba50dcc7738c295f76a603f2b5ff2155aec4d69
Marc Jakobi <jakobi@htw-berlin.de>
2017-01-12 13:00:55 +0100

    added curvefitCollectionTests

---
+++ MLUnitTests/curvefitCollectionTests/curvefitCollectionTests.m
@@ -0,0 +1,12 @@
+function curvefitCollectionTests(fig)
+%CURVEFITCOLLECTIONTESTS MLUnit tests for curvefit collections
+import lfpBattery.*
+if nargin < 1
+    fig = false;
+end
+
+%% MTODO test functionality of error handling in add() and remove() methods
+
+%% MTODO validate spline interpolation of dischargeCurves
+end
+
