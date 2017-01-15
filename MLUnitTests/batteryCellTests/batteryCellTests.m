b592e1b31c9da93d8a8bebc0a5b305f6c8cb6b79
Marc Jakobi <jakobi@htw-berlin.de>
2017-01-15 19:29:47 +0100

    syntax cleanup

--- MLUnitTests/batteryCellTests/batteryCellTests.m
+++ MLUnitTests/batteryCellTests/batteryCellTests.m
@@ -11,12 +11,12 @@ b.adddcurves(d)
 dt = 60;
 
 %% Charge and discharge with current and SoC limiting tests
-P = b.powerRequest(-10, dt);
+b.powerRequest(-10, dt);
 assert(isequal(b.SoC, 0.2), 'Unexpected lower SoC limitation.')
 P = b.powerRequest(500, dt);
 assert(P < 100, 'Unexpected current limitation.')
 for i = 1:100
-    P = b.powerRequest(60, dt);
+    b.powerRequest(60, dt);
 end
 assert(isequal(b.SoC, 1), 'Unexpected upper SoC limitation')
 
