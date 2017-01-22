46d50cbee68061ec046854a459f2056fa7a0844f
Marc Jakobi <jakobi@htw-berlin.de>
2017-01-22 16:22:59 +0100

    Began work on batteryPack

--- +lfpBattery/@batteryPack/batteryPack.m
+++ +lfpBattery/@batteryPack/batteryPack.m
@@ -1,33 +1,84 @@
-classdef batteryPack < lfpBattery.batteryInterface & lfpBattery.composite
-    %BATTERYPACK: Model of lithium iron phosphate battery pack containing
-    %multiple cells
+classdef batteryPack < lfpBattery.batteryInterface
+    %BATTERYPACK: Cell-resolved model of a lithium ion battery pack.
+    %
     
-    properties (SetAccess = 'immutable')
-        ageModelLevel = 'pack'; % 'pack' or 'cell'
+    properties (Dependent)
+        V; % Resting voltage of the battery pack in V
     end
-    properties (Access = 'protected')
-        cells; % batteryCell objects % MTODO: create classes for different topologies (PS, SP,...)
+    properties (Dependent, SetAccess = 'protected')
+        % Discharge capacity in Ah (Cd = 0 if SoC = 1).
+        % The discharge capacity is given by the nominal capacity Cn and
+        % the current capacity C at SoC.
+        % Cd = Cn - C
+        Cd;
+        % Current capacity level in Ah.
+        C;
+    end
+    properties (Dependent, SetAccess = 'immutable')
+        % Internal impedance of the battery pack in Ohm.
+        % The internal impedance is currently not used as a physical
+        % parameter. However, it is used in the circuit elements
+        % (seriesElement/parallelElement) to determine the distribution
+        % of currents and voltages.
+        Zi;
     end
     
     methods
         function b = batteryPack(varargin)
             b@lfpBattery.batteryInterface(varargin{:})
+        end % constructor
+        function addcurves(b, d)
+            b.pass2cells(@addcurves, d);
         end
-%         function powerRequest(b, P, dt)
-            % MTODO: implement charging function here
-%         end
-%         function adddfit(b, d)
-            % MTODO: add discharge fit handle to cells
-%             b.findImax();
-%         end
-%         function adddcurves(b, d)
-            % MTODO: add discharge curve handle to cells
-            %b.findImax();
-%         end
-%         function it = createIterator(b)
-%            % MTODO: create iterator class and implement this method
-%         end
+        function v = getNewVoltage(b, I, dt)
+            v = b.El.getNewVoltage(I, dt);
+        end
+        %% getters & setters
+        function set.V(b, v) %#ok<INUSD>
+            % Cannot protect SetAcces any other way due to shared interface.
+            error('Cannot set read-only property V in batteryPack.')
+        end
+        function v = get.V(b)
+            v = b.El.V;
+        end
+        function c = get.Cd(b)
+            c = b.El.Cd;
+        end
+        function c = get.C(b)
+            c = b.El.C;
+        end
+        function z = get.Zi(b)
+            z = b.El.Zi;
+        end
     end
     
+    methods (Access = 'protected')
+        function pass2cells(b, fun, varargin)
+            %PASS2CELLS: Passes the function specified by function handle fun
+            %to all cells in this pack
+            it = b.El.createIterator;
+            while it.hasNext
+                cell = it.next;
+                feval(fun, cell, varargin{:});
+            end
+        end
+        % Abstract methods passed on to El handle
+        function charge(b, Q)
+            b.El.charge(Q)
+        end
+        function c = dummyCharge(b, Q)
+            c = b.El.dummyCharge(Q);
+        end
+        function refreshNominals(b)
+            b.El.refreshNominals;
+        end
+        function s = sohCalc(b)
+            s = b.El.sohCalc;
+        end
+        function i = findImax(b)
+            i = b.El.findImax;
+            b.Imax = i;
+        end
+    end
 end
 
--- +lfpBattery/@circuitFactory/circuitFactory.m
+++ +lfpBattery/@circuitFactory/circuitFactory.m
@@ -1,11 +1,9 @@
-classdef circuitFactory
-    %CIRCUITFACTORY Summary of this class goes here
-    %   Detailed explanation goes here
-    
-    properties
-    end
-    
+classdef circuitFactory < handle
+    %CIRCUITFACTORY: Object for creating pre-defined batteryCell topologies
+
     methods
+        function el = parallelStringsPE(Vpack, Vcell, Cpack, Ccell, Zcell)
+        end
     end
     
 end
